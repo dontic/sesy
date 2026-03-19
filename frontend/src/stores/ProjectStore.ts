7fafd9afa55838b5f9b545609a6c739ce01daf26
Daniel Garcia <github@mail.daniel.es>
2026-03-19 11:45:50 +0000

    frontend: Fix bug reseting the project selector on page change

--- frontend/src/stores/ProjectStore.ts
+++ frontend/src/stores/ProjectStore.ts
@@ -14,10 +14,15 @@ export const useProjectStore = create<ProjectStore>((set) => ({
   projects: [],
   currentProject: undefined,
   setProjects: (projects) =>
-    set({
+    set((state) => ({
       projects,
-      currentProject: projects.length > 0 ? projects[0] : undefined,
-    }),
+      currentProject:
+        state.currentProject !== undefined
+          ? projects.find((p) => p.pk === state.currentProject!.pk) ?? (projects.length > 0 ? projects[0] : undefined)
+          : projects.length > 0
+          ? projects[0]
+          : undefined,
+    })),
   setCurrentProject: (project) => set({ currentProject: project }),
   clearProjects: () => set({ projects: [], currentProject: undefined }),
 }));
