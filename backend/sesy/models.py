d6a59322d6ccd45b9ee86693e99c086daeba8d1f
Daniel Garcia <github@mail.daniel.es>
2026-03-19 09:46:56 +0000

    backend: Change Project model ordering to ascending by created_at

--- backend/sesy/models.py
+++ backend/sesy/models.py
@@ -44,7 +44,7 @@ class Project(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["created_at"]
 
     def __str__(self):
         return self.name
